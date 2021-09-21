b2e2cdc4a31a498a2b0e0c0bf52f0de8b7fe247d
Phenix-G <eiphenixg@gmail.com>
2021-09-21 22:52:41 +0800

    feat(models): add user other model

--- models/user.py
+++ models/user.py
@@ -6,7 +6,6 @@
 from typing import Optional
 from uuid import uuid4, uuid5, UUID
 
-from pydantic import EmailStr
 from sqlmodel import Boolean, Column, Field, SQLModel, String
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -16,7 +15,7 @@ from .base import Base, TimeStampMixin
 class UserBase(Base, TimeStampMixin):
     uuid: UUID = Field(sa_column=Column(String(36), unique=True, nullable=False))
     username: str = Field(sa_column=Column(String(20), unique=True, index=True, nullable=False, comment='用户名'))
-    email: Optional[EmailStr] = Field(
+    email: Optional[str] = Field(
         sa_column=Column(String(40), unique=True, index=True, nullable=True, comment='邮箱')
     )
     password: str = Field(sa_column=Column(String(102), nullable=False, comment='密码'))
@@ -28,7 +27,23 @@ class UserBase(Base, TimeStampMixin):
 
 
 class User(UserBase, table=True):
-    pass
+    def save(self, obj):
+        self.__generate_uuid()
+        self.__make_password(self.password)
+        super().save(self)
+
+    def set_password(self, raw_password):
+        return self.__make_password(raw_password)
+
+    def __make_password(self, raw_password):
+        self.password = generate_password_hash(raw_password, method="pbkdf2:sha256:216000", salt_length=16)
+
+    def check_password(self, raw_password: str) -> bool:
+        return check_password_hash(self.password, raw_password)
+
+    def __generate_uuid(self):
+        namespace = uuid4()
+        self.uuid = uuid5(namespace, self.username)
 
 
 class UserCreate(SQLModel):
@@ -39,15 +54,21 @@ class UserCreate(SQLModel):
     description: Optional[str]
 
 
-class UserRead(UserBase):
+class UserRead(SQLModel):
     id: int
-    description: str
+    uuid: UUID
+    username: str
     email: str
+    phone: str
+    description: str
+    is_active: bool
+    is_admin: bool
+    ip_address: str
 
 
 class UserUpdate(SQLModel):
     username: Optional[str] = None
-    email: Optional[EmailStr] = None
+    email: Optional[str] = None
     password: Optional[str] = None
     phone: Optional[str] = None
     description: Optional[str] = None
