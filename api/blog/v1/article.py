0dcc9a437e2c9ff01bbfd8e9e9eb485ef700a600
Phenix-G <eiphenixg@gmail.com>
2021-09-21 22:55:47 +0800

    feat(api): rebuild articles

---
+++ api/blog/v1/article.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Phenix-G
+# @File   : article.py
+# @Time   : 2021/09/19 14:45
+from fastapi import APIRouter
+
+from models.article import ArticleCreate, Article, ArticleUpdate
+
+router = APIRouter()
+
+
+@router.get('/')
+async def get_list():
+    articles = Article.all()
+    return {'data': articles}
+
+
+@router.get('/{article_id}')
+async def get(article_id: int, ):
+    db_article = Article.get(article_id)
+    return {'data': db_article}
+
+
+@router.post('/')
+async def create(obj: ArticleCreate):
+    article = Article.create(**obj.dict())
+    return {'data': article}
+
+
+@router.put('/{article_id}')
+async def update(article_id: int, obj: ArticleUpdate):
+    article = Article.get(article_id)
+    data = obj.dict(exclude_unset=True)
+    article.update(data)
+    return {'data': article}
+
+
+@router.delete('/{article_id}')
+async def delete(article_id: int):
+    article = Article.get(article_id)
+    article.row_delete()
+    return {'data': ''}
--- api/blog/v1/post.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python3
-# -*- coding: utf-8 -*-
-# @Author : Phenix-G
-# @File   : post.py
-# @Time   : 2021/07/07 0:25
