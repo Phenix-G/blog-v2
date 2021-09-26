a0e0b7ab08d0f5d4df0a58247494d401dc3c76a7
Phenix-G <eiphenixg@gmail.com>
2021-09-27 00:24:40 +0800

    refactor(core): fix circular import

---
+++ core/settings/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author : Phenix-G
+# @File   : __init__.py
+# @Time   : 2021/09/27 0:01
+from .db import session
--- core/settings/config.py
+++ core/settings/config.py
@@ -8,7 +8,7 @@
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 class Config:
